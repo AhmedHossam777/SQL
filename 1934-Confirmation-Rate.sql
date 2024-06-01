c1562cff7ecb4cdb0736a572b3b8a7e79d07c9bf
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-06-01 06:40:10 +0300

    [1934] [Time Beats: 90.22%] - LeetPush

---
+++ 1934-Confirmation-Rate.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select s.user_id,  round(avg(if(c.action="confirmed",1,0)),2) as confirmation_rate
+from Confirmations as c
+right join Signups as s on s.user_id = c.user_id
+group by s.user_id
+
+-- (select action from Confirmations where action='confirmed')
